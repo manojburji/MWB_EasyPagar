61caa8912c2ac24a12f0b0f493d7c3e0d8bab3f1
manojburji <manojburji05@gmail.com>
2025-11-26 20:29:03 +0530

    Remove the extra folders added

--- MWB_Enterprises/src/test/java/Enterprises/Homepage.java
+++ MWB_Enterprises/src/test/java/Enterprises/Homepage.java
@@ -11,42 +11,36 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-	public class Homepage 
-	{
-		WebDriver driver;
-		private WebDriverWait wait;
+public class Homepage {
 
-		public Homepage(WebDriver driver) 
-		{
-			this.driver = driver;
-			this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-			PageFactory.initElements(driver, this);
-		}
-		
-		@FindBy(xpath = "//label[@for=\"Dashboard\"]")
-		WebElement Dashboard;
-		
-		@FindBy(xpath = "//label[@for='Leave']")
-		WebElement LeavePolicy;
-		
-		@FindBy(xpath = "//label[@for=\"Holidays\"]")
-		WebElement Holiday;
-		
-		
-		
-		public void dashboard()
-		{
-			Dashboard.click();
-		}
-		
-		public void leavePolicy()
-		{
-		    WebElement leavePolicy = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//label[@for='Leave']")));
-		    ((JavascriptExecutor) driver).executeScript("arguments[0].click();", leavePolicy);
-		}
-		
-		public void holiday() throws InterruptedException
-		{
-			Holiday.click();
-		}
+    WebDriver driver;
+    private WebDriverWait wait;
+
+    public Homepage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(xpath = "//label[@for=\"Dashboard\"]")
+    WebElement Dashboard;
+
+    @FindBy(xpath = "//label[@for='Leave']")
+    WebElement LeavePolicy;
+
+    @FindBy(xpath = "//label[@for=\"Holidays\"]")
+    WebElement Holiday;
+
+    public void dashboard() {
+        Dashboard.click();
+    }
+
+    public void leavePolicy() {
+        WebElement leavePolicy = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//label[@for='Leave']")));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", leavePolicy);
+    }
+
+    public void holiday() throws InterruptedException {
+        Holiday.click();
+    }
 }
