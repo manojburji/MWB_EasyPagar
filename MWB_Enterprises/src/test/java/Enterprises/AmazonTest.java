30b99ad74a2683d79cfbd1a92952c38f1ae6f0ea
manojburji <manojburji05@gmail.com>
2025-11-27 15:22:49 +0530

    Jenkins Test

--- MWB_Enterprises/src/test/java/Enterprises/AmazonTest.java
+++ MWB_Enterprises/src/test/java/Enterprises/AmazonTest.java
@@ -2,28 +2,27 @@ package Enterprises;
 
 import org.testng.annotations.Test;
 
-import EnterprisesPOM.Amazon;
+public class AmazonTest extends HRMSBaseClass {
 
-public class AmazonTest extends HRMSBaseClass
-{
-	@Test(description = "Amazon Product Price and Ratings")
-	public void Price() throws InterruptedException
-	{
-		Amazon.searchBar();
-		Amazon.searchButton();
-		Amazon.clickOnMobile();
-		Amazon.productPrice();
-		Amazon.productRatings();
-	}
-	
-	@Test(description = "Flipkart Price Ratings and Reviews")
-	public void Ratings() throws InterruptedException
-	{
-		Amazon.flipkartsearchbar();
-		Amazon.flipkartClickOnMobiles();
-		Amazon.flipkartProductPrice();
-		Amazon.flipkartProductRatingsAndReviews();
-	}
+    @Test(description = "Amazon Product Price and Ratings")
+    public void Price() throws InterruptedException {
+        Amazon.searchBar();
+        Amazon.searchButton();
+        Amazon.clickOnMobile();
+        Amazon.productPrice();
+        Amazon.productRatings();
+    }
 
-	@Test
+    @Test(description = "Flipkart Price Ratings and Reviews")
+    public void Ratings() throws InterruptedException {
+        Amazon.flipkartsearchbar();
+        Amazon.flipkartClickOnMobiles();
+        Amazon.flipkartProductPrice();
+        Amazon.flipkartProductRatingsAndReviews();
+    }
+
+    @Test(description = "Test")
+    public void RatingsTest() {
+
+    }
 }
