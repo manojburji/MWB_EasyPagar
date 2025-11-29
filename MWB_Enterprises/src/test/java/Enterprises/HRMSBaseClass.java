19e3aca6027cf4c47d0ca2951fd5ed328613f648
manojburji <manojburji05@gmail.com>
2025-11-29 06:26:24 +0530

    Automatically Trigerring with pass and fail test cases

--- MWB_Enterprises/src/test/java/Enterprises/HRMSBaseClass.java
+++ MWB_Enterprises/src/test/java/Enterprises/HRMSBaseClass.java
@@ -4,7 +4,7 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import EnterprisesPOM.Amazon;
@@ -46,7 +46,7 @@ public class HRMSBaseClass {
     public LeavePolicy LeavePolicy;
     public Amazon Amazon;
 
-    @AfterClass
+    @AfterMethod
     public void tearDown() {
         if (driver != null) {
             driver.quit();
